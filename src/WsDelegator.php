13416fceaa7848bfd2c31c604b49a72a0e0ab017
Mahmut Bayri <mahmutbayri@yahoo.com>
2020-11-09 09:17:36 +0300

    first commit

---
+++ src/WsDelegator.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MahmutBayri\WebSocketDelegator;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use SplObjectStorage;
+
+class WsDelegator implements MessageComponentInterface
+{
+    protected SplObjectStorage $clients;
+    public ?\Closure $onConnect = null;
+    public ?\Closure $onMessage = null;
+    public ?\Closure $onClose = null;
+
+    public function __construct()
+    {
+        $this->clients = new SplObjectStorage();
+    }
+
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $this->clients->attach($conn);
+        $this->setConnectionName($conn, 'connection_' . $conn->resourceId);
+        echo "New connection! ({$conn->resourceId})\n";
+        $this->onConnect($conn);
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     * @param string $msg
+     */
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
+        $this->onGetMessage($msg);
+    }
+
+    public function onClose(ConnectionInterface $conn)
+    {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->onClose($this->getConnectionName($conn));
+        echo "Connection {$conn->resourceId} has disconnected\n";
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+
+    public function sendMessage($message)
+    {
+        foreach ($this->clients as $client) {
+            /** @var \Ratchet\WebSocket\WsConnection $client */
+            $client->send($message);
+        }
+    }
+
+    public function getConnectionName(ConnectionInterface $connection)
+    {
+        return $this->clients->offsetGet($connection);
+    }
+
+    private function setConnectionName(ConnectionInterface $connection, $name)
+    {
+        $this->clients->offsetSet($connection, $name);
+    }
+}
